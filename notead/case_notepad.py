96e535c22af7eaf0b504d20656fe8e0aa90e5481
sabersame <38524798+sabersame@users.noreply.github.com>
2020-01-16 14:15:59 +0800

    Update case_notepad.py

--- notead/case_notepad.py
+++ notead/case_notepad.py
@@ -9,21 +9,21 @@ class Case_Notepad(Base_Base):
         super().__init__()
 
     def edittext_input_input(self, case_name, text):
-        self.input(case_name, "Edit", "ClassName", "Edit", "1", text)
+        self._input(case_name, "Edit", "ClassName", "Edit", "1", text)
         # 如果想光标快速移动到当前行的最前面/最后面, 可以传入{Home}/{End}
         # 换行的话可以传入{Enter}
 
     def edittext_input_click(self, case_name):
-        self.click(case_name, "Edit", "ClassName", "Edit", "1")
+        self._click(case_name, "Edit", "ClassName", "Edit", "1")
 
     def restore_button_click(self, case_name):
-        self.click(case_name, "Button", "Name", "还原", "2")
+        self._click(case_name, "Button", "Name", "还原", "2")
 
     def close_button_click(self, case_name):
-        self.click(case_name, "Button", "Name", "关闭", "2")
+        self._click(case_name, "Button", "Name", "关闭", "2")
 
     def notsave_button_click(self, case_name):
-        self.click(case_name, "Button", "Name", "不保存(N)", "3")
+        self._click(case_name, "Button", "Name", "不保存(N)", "3")
         # 当然也可以使用键盘输入模拟选择
         # self.input(case_name, "Button", "Name", "不保存(N)", "3", {Alt}n)
 
